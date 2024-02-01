e003de6e8aa5d1eb90d3f6ff3debcf419945942d
mohajer <senator.x.x85@gmail.com>
2024-02-01 13:47:33 +0330

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Custom404() {
   return (
     <div>
-      <h1>Not Found Page! **ERROR 404!**</h1>
+      <h1>Can't Found Page! **ERROR 404!**</h1>
       <img src="../public/favicon.ico" alt="icon" />
     </div>
   )
