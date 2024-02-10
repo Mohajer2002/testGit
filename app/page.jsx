9ea1964cf2ce64a398c42d4a78c59aa3041ea97a
mohajer <senator.x.x85@gmail.com>
2024-02-10 18:22:14 +0330

    not found page

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,18 @@
+"use client";
 import Link from "next/link";
 
 export default function Home() {
-  
   return (
     <div>
-      <ul>
+      <ul className="flex justify-center items-center gap-x-10 text-5xl text-red-400 ">
         <li>
-          <Link href="/about"> home</Link>
+          <Link href="/about"> About</Link>
+        </li>
+        <li>
+          <Link href="/about/12"> About/12</Link>
+        </li>
+        <li>
+          <Link href="/test"> Test</Link>
         </li>
       </ul>
     </div>
